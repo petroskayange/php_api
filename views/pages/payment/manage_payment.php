1ed494ec4213411057c418a3aa66019f271ed0f7
petroskayange <kayangepetros@gmail.com>
2022-05-14 13:21:52 +0200

    fixing payment page

--- views/pages/payment/manage_payment.php
+++ views/pages/payment/manage_payment.php
@@ -86,7 +86,7 @@
 <!-- Page specific script -->
 <script src="../../dist/js/backend.js"></script>
 <script>
-  var url = "<?=$_SESSION['base_url']?>"+"php_api/api/post/read.php";
+  var url = "<?=$_SESSION['base_url']?>"+"api/post/read.php";
   getData(url,'displayData')
 
   function displayData(data){
